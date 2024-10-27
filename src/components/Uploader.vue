57be505e98e3d3276616e110ed255bad17f82445
Jamie Sullivan <jkcrawshaw@gmail.com>
2024-10-27 10:09:44 -0400

    adds uploader

---
+++ src/components/Uploader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="csv-uploader">
+    <input type="file" @change="handleFileUpload" accept=".csv" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["handleFileUpload"],
+};
+</script>
